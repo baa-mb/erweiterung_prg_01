abddee331b6853d440a6eefa92a8bcf36becc7c0
baa <34665120+baa-mb@users.noreply.github.com>
2021-03-12 13:17:22 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+
+//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
 namespace baaFunktionen {
    basic.showNumber(11);
-    basic.showIcon(IconNames.Heart)
+   
+   //% block
+    export function showNumber(v: number, interval: number = 150): void {
+        basic.showNumber(v)
+    }
 }
