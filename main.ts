d058fd9bcfea539e40966dcccad3da9a66b88b4e
baa <34665120+baa-mb@users.noreply.github.com>
2021-03-12 13:40:02 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ basic.showIcon(IconNames.Heart)
 /**
  * Provides access to basic micro:bit functionality.
  */
-//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
+//% color=190 weight=100 icon="\uf1ec" block="BAA Blocks"
 namespace baaFunc {
    basic.showNumber(0);
    
